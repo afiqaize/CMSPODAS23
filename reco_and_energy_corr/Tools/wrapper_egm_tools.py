c20e8794922ff82eb0d2de8725e74cb3c0133a7a
Afiq Anuar <afiq.anuar@cern.ch>
2023-10-03 12:03:55 +0200

    smh these canvas bs

--- reco_and_energy_corr/Tools/wrapper_egm_tools.py
+++ reco_and_energy_corr/Tools/wrapper_egm_tools.py
@@ -8,17 +8,38 @@ for src in ['CruijffPdf.cc', 'EGUtilFuncs.cc', 'GBRMath.cc', 'HistFuncs.cc', 'Ma
     with open(f'{egm_tools_base}/src/{src}') as sfile:
         cppyy.cppdef(sfile.read())
 
+from ROOT import TCanvas
 from cppyy.gbl import ResFitter
 from cppyy.gbl.std import vector
+
+class NotebookHistogram(object):
+    canvas = None
+    fitter = None
+
+    def __init__(self, histogram):
+        self.histogram = histogram
+
+    def Draw(self, option = None):
+        if canvas is None:
+            canvas = TCanvas()
+        self.histogram.Draw()
+        canvas.Draw()
+
+    @classmethod
+    def reset(cls, x):
+        del cls.canvas
+        cls.canvas = None
+        del cls.fitter
+        cls.fitter = None
 
 def makeChain(cname, flist, njob = 1, ijob = 1, verbose = 2):
     return cppyy.gbl.HistFuncs.makeChain(cname, flist, njob, ijob, verbose)
 
 def makeHist(tree, nbin, xmin, xmax, variable, cut):
-    return cppyy.gbl.HistFuncs.makeHist(tree, nbin, xmin, xmax, variable, cut)
+    return NotebookHistogram(cppyy.gbl.HistFuncs.makeHist(tree, nbin, xmin, xmax, variable, cut))
 
 def compVars(tree, nbin, xmin, xmax, variables, cut):
-    return cppyy.gbl.HistFuncs.compVars(tree, nbin, xmin, xmax, vector[str](variables), cut)
+    return NotebookHistogram(cppyy.gbl.HistFuncs.compVars(tree, nbin, xmin, xmax, vector[str](variables), cut))
 
 def compareDataMC(h_data, h_mc, normalize):
-    return cppyy.gbl.HistFuncs.compareDataMC(h_data, h_mc, normalize)
+    return NotebookHistogram(cppyy.gbl.HistFuncs.compareDataMC(h_data.histogram, h_mc.histogram, normalize))
